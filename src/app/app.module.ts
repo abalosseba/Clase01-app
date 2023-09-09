8ac68d21f9dfc569166246273974da1e532f169e
Sebastian Abalos <arnaldosebastianabalos@gmail.com>
2023-09-08 22:04:55 -0300

    Componentes nuevos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { BienvenidosComponent } from './components/bienvenidos/bienvenidos.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidosComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
