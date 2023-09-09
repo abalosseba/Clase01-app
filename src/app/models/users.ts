3f9cd96a8aa68fb7a76370c4a6426f3b6b4eca1c
Sebastian Abalos <arnaldosebastianabalos@gmail.com>
2023-09-09 11:16:44 -0300

    Clase creada

---
+++ src/app/models/users.ts
@@ -0,0 +1,11 @@
+export class Users {
+    name: string;
+    pass : string;
+
+
+    constructor(nameUser: string, passwordUser: string){
+        this.name=nameUser;
+        this.pass= passwordUser;
+    }
+
+}
