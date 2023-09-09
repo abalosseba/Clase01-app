b2b0c718bc023063e4f31dd6cbe6568154340504
Sebastian Abalos <arnaldosebastianabalos@gmail.com>
2023-09-08 21:59:18 -0300

    Ejercicio 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Clase01-app';
+  titulo: string ='Ejercicio Clase 1'
+  ejercio1: string ='Calculos de edades'
+  edad1:number = 0;
+  edad2:number = 0;
+  suma:number = 0;
+  promedio: number = 0;
+
+  calcular(){
+    this.suma= this.edad1+this.edad2;
+    this.promedio=this.suma/2
+  }
+limpiar(){
+  this.edad1= 0;
+  this.edad2= 0;
+  this.suma = 0;
+  this.promedio = 0;
+}
 }
